bdf5e6976af13341ec50c0c0951e1bfbbae6e2b1
ymf359 <1137626035@qq.com>
2022-08-26 14:29:08 +0800

    加入了创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/androidweather/db/City.java
@@ -0,0 +1,39 @@
+package com.example.androidweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+    private void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
